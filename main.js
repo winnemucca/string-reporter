8013bec67310ae25e1f41f10da2569e2b8de9078
Steve Kauyedauty <skauyedauty@gmail.com>
2014-06-23 16:08:52 -0600

    1st commit

---
+++ main.js
@@ -0,0 +1,8 @@
+var word=prompt("enter any word");
+console.log(word);
+console.log(word.length);
+console.log(word.charAt(2));
+console.log(word.toLowerCase());
+console.log(word.toUpperCase());
+console.log("I am so hungry for a " + word);
+console.log(word.substring(1,4));
